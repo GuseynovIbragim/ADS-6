ce5208505219476193dadd2c838147d5a1b5131d
GuseynovIbragim <78927590+GuseynovIbragim@users.noreply.github.com>
2021-05-28 10:46:21 +0300

    Гусейнов

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,16 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-
+template <class T>
+class BST {
+T data;
+int count;
+BST<T> *left, *right;
+ public:
+BST();
+BST(T);
+BST<T>* Insert(BST<T>*, T);
+void Inorder(BST<T>*);
+BST<T>* makeTree(const char *filename);
+};
 #endif  // INCLUDE_BST_H_
