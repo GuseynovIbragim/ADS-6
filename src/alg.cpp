cd02b5db7ad2dae5ecf0d0eb3050d030c0c9a9e0
GuseynovIbragim <78927590+GuseynovIbragim@users.noreply.github.com>
2021-05-28 10:53:30 +0300

    Гусейнов

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,25 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+ifstream file(filename);
+
+if (!file) {
+cout << "error opening file!" << std::endl;
+return NULL;
+}
+BST<string> b, *root = NULL;
+string word = "";
+while (!file.eof()) {
+int ch = file.get();
+if (isalpha(ch)) {
+word += ch;
+} else {
+if (word != "") {
+root = b.Insert(root, toLowerCase(word));
+word = "";
+}
+}
+}
+file.close();
+return root;
 }
